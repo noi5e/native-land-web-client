fe57c96269f4d1c5fc1d313b9ba95415df1b4092
noi5e <will.gutierrez@gmail.com>
2024-05-26 18:58:12 -0700

    add wait time before lost pixel screenshot

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -7,7 +7,7 @@ export const config: CustomProjectConfig = {
   // OSS mode
   generateOnly: true,
   failOnDifference: true,
-
+  waitBeforeScreenshot: 30000,
   // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
   // lostPixelProjectId: "xxxx",
   // process.env.LOST_PIXEL_API_KEY,
