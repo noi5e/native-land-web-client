34f97692d9a381827979c75475e18fd781cf045a
noi5e <will.gutierrez@gmail.com>
2024-05-29 00:10:22 -0700

    introduce wait time

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -7,7 +7,7 @@ export const config: CustomProjectConfig = {
   // OSS mode
   // generateOnly: true,
   // failOnDifference: true,
-  // waitBeforeScreenshot: 30000,
+  waitBeforeScreenshot: 5000,
   // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
   lostPixelProjectId: "clwrdm5tv1b1ty9qrfr9560g2",
   apiKey: process.env.LOST_PIXEL_API_KEY,
