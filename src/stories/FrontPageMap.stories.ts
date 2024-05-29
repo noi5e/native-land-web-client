410e35036b57cb61e5b107d112a26fbb0a2f9833
noi5e <will.gutierrez@gmail.com>
2024-05-29 10:24:54 -0700

    center honduras in visual tests

--- src/stories/FrontPageMap.stories.ts
+++ src/stories/FrontPageMap.stories.ts
@@ -1,4 +1,5 @@
 import FrontPageMap from "../FrontPageMap";
+// import { within, userEvent } from "@storybook/test";
 import {
   withRouter,
   reactRouterParameters,
@@ -12,12 +13,14 @@ export default {
     reactRouter: reactRouterParameters({
       routing: { path: "/" },
       location: {
-        // enter searchParams to simulate a http://localhost:6006/?longitude=-100.1953125&latitude=47.27922900257082
+        // enter searchParams to simulate a http://localhost:5173/?longitude=-87.25571145903918&latitude=14.065549509301533
         // this is so that we can test the map at a specific, repeatable location every time
         // if searchParams aren't given, the default is to randomly pick 1 of 3 coordinates
         searchParams: {
-          longitude: "-100.1953125",
-          latitude: "47.27922900257082",
+          // this centers honduras
+          // so chosen because the developers' tileset includes two overlapping polygons in this location
+          longitude: "-87.25571145903918",
+          latitude: "14.065549509301533",
         },
       },
     }),
@@ -28,4 +31,20 @@ export const Primary = {
   args: {
     navBarHeight: "6rem",
   },
+  // play: async ({ canvasElement }) => {
+  //   const canvas = within(canvasElement);
+
+  //   await (async () => {
+  //     setTimeout(() => {
+  //       userEvent.type(canvas.getByTestId("woot"), "HERE IS THE TEXT!!!!!");
+  //     }, 15000);
+  //   });
+  // },
 };
+
+// export const ZoomedIn = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement);
+
+//   }
+// }
