95eaeed0a453c36bfc0efb8fde1b15317f8f685a
noi5e <will.gutierrez@gmail.com>
2024-05-28 22:25:17 -0700

    setup lost pixel platform edition

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -5,9 +5,10 @@ export const config: CustomProjectConfig = {
     storybookUrl: "./storybook-static",
   },
   // OSS mode
-  generateOnly: true,
-  failOnDifference: true,
+  // generateOnly: true,
+  // failOnDifference: true,
+  // waitBeforeScreenshot: 30000,
   // Lost Pixel Platform (to use in Platform mode, comment out the OSS mode and uncomment this part )
-  // lostPixelProjectId: "xxxx",
-  // process.env.LOST_PIXEL_API_KEY,
+  lostPixelProjectId: "clwrdm5tv1b1ty9qrfr9560g2",
+  apiKey: process.env.LOST_PIXEL_API_KEY,
 };
